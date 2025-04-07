0966d5dafb5a50121d82dd1a7934a357c7262d01
Adam Gaia <git@agaia.dev>
2025-04-07 13:13:46 -0600

    fix: disable typos via treefmt for now

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -9,6 +9,6 @@
     jsonfmt.enable = true; # json
     yamlfmt.enable = true; # yaml
     taplo.enable = true; # toml
-    #typos.enable = true; # spellcheck
+    #typos.enable = true; # spellcheck # Disabled until https://github.com/numtide/treefmt/issues/198 is fixed
   };
 }
